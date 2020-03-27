c3c49d8282c48a98da7badaae30d9c5d8cbd70b5
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2020-03-26 22:22:12 -0600

    Updates

---
+++ precache-manifest.71eebad8c11d32a4335ffb25dcf6baae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4036cedb9a0ad698da17fbfab97ca8c",
+    "url": "/react-recipe/index.html"
+  },
+  {
+    "revision": "ae7ae25e423ceedcebe4",
+    "url": "/react-recipe/static/css/main.cb8f5613.chunk.css"
+  },
+  {
+    "revision": "6e6399b281eb67a792f7",
+    "url": "/react-recipe/static/js/2.0a6a514c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recipe/static/js/2.0a6a514c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae7ae25e423ceedcebe4",
+    "url": "/react-recipe/static/js/main.2a56d80c.chunk.js"
+  },
+  {
+    "revision": "a1f4ee5bfefb1caabe24",
+    "url": "/react-recipe/static/js/runtime-main.292a16c0.js"
+  }
+]);
